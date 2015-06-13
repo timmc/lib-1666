485ba83ea224759eca12b7bc368ae5240a0da9b7
Tim McCormack <cortex@brainonfire.net>
2015-06-13 08:10:56 -0400

    base

---
+++ README.md
@@ -0,0 +1,7 @@
+# lib-1666
+
+A Clojure proof of concept for username autocompletion in euphoria-io/heim.
+
+## License
+
+Copyright © 2015 Tim McCormack, released under the WTFPL v2.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject lib-1666 "0.1.0-SNAPSHOT"
+  :description "PoC for heim nick autocompletion."
+  :url "https://github.com/timmc/lib-1666"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :repl-options {:init-ns lib-1666.core})
