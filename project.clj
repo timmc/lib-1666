0036b6cdb5edff5ac32dfd926249eee0a977c7c1
Tim McCormack <cortex@brainonfire.net>
2015-06-13 09:31:40 -0400

    comment quickie

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/timmc/lib-1666"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[quickie "0.3.11"]]
-  :test-matcher #"lib-1666(\.|\..*\.)t-.*"
+  :test-matcher #"lib-1666(\.|\..*\.)t-.*" ;; for quickie
   :repl-options {:init-ns lib-1666.core})
