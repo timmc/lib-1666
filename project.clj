a9507a1d6be74c7316ec11a98148f9d65d0f266a
Tim McCormack <cortex@brainonfire.net>
2015-06-13 08:34:23 -0400

    `lein quickie` now working

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "PoC for heim nick autocompletion."
   :url "https://github.com/timmc/lib-1666"
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[quickie "0.3.11"]]
+  :test-matcher #"lib-1666(\.|\..*\.)t-.*"
   :repl-options {:init-ns lib-1666.core})
